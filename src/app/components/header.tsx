628198f30bc3ac2771d18ae3c14318f4127239cc
Mega Afu <[mega.afu@champier.co.mz]>
2023-05-16 16:41:20 +0200

    [MotorCare] refacture: Change the design of Footer

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -18,29 +18,28 @@ const header = () => {
   }
 
   return (
-    <header className=' mt-11'>      
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <header className=' mt-11 absolute w-full z-50'>      
+      <nav className="bg-white border-gray-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center">
               <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
           </a>
           <div className="flex items-center md:order-2">
             <LanguageSwitcher/>
           </div>
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-language-select">
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
               { menu.map((item, index) => {
                 return (
                   selectMenu === item ?
                   <li key={index}>
-                    <a href="#" className="group text-sky-700 after:text-sky-600 transition duration-300">
+                    <a href="#" className="group text-sky-700 after:text-sky-600 transition duration-300 md:text-md lg:text-lg">
                       {item}
                       <span className="mt-1 block max-w-full h-0.5 bg-sky-600"></span>
                     </a>
                   </li>
                   : <li key={index}>
-                  <a href="#" onClick={() => handleMenu(item)} className="group text-gray-700 after:text-sky-600 transition duration-300">
+                  <a href="#" onClick={() => handleMenu(item)} className="group text-gray-700 after:text-sky-600 transition duration-300 md:text-md lg:text-lg">
                     {item}
                     <span className="mt-1 block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
                   </a>
