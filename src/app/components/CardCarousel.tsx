8c8ac639a75618a88fcac81a82289d2d1b8a47b5
Mega Afu <[mega.afu@champier.co.mz]>
2023-05-18 14:39:03 +0200

    [MotorCare] refacture: Added style and icons to CaousellCard

--- src/app/components/CardCarousel.tsx
+++ src/app/components/CardCarousel.tsx
@@ -15,33 +15,42 @@ interface CardCarouselProps {
 const CardCarousel: React.FC<CardCarouselProps> = ({ images, title, description, price ,date, combustivel,kilometragem }) => {
   
     return (
-      <div className="max-w-sm bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm bg-white border border-gray-200  shadow rounded-lg overflow-hidden">
         
-        <Carousel autoPlay={true} showThumbs={false} showStatus={false} infiniteLoop={true} >
+        <Carousel autoPlay={true} showThumbs={false} showStatus={false} infiniteLoop={true} dynamicHeight={false} showIndicators={true} showArrows={false} stopOnHover={false} swipeable={true} emulateTouch={true} interval={5000} transitionTime={500}>
           {
             images.map((image, index) => {
                 return(
-                  <div className='flex'>
-                    <img src={image} />
+                  <div className='h-[240px] w-full' key={index} bg-gray-200 >
+                    <img src={image} alt="image" className="rounded-t-lg "/>
                   </div>
                 )
             }
           )}
         </Carousel>
   
-        <div className="p-5">
-          <h3 className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900'>{title}</h3>
-        <div>
-        <p className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900'>{description}</p>
-        <p className='inline-flex items-center px-4 py-2 text-sm font-medium text-center   text-[#FF0000]'>{price}</p>
-        </div>
-          <hr className='border-1 border-[#272424] '></hr>
-          <div className='display: inline'>
-          <p className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900' >{date}</p>
-          <p className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900'>{combustivel}</p>
-          <p className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900'>{kilometragem}</p>
+        <div className="px-4 pb-6">
+          <h3 className=' text-xl font-bold '>{title}</h3>
+          <div className='flex justify-between py-2 '>
+            <p className=' text-lg text-gray-700'>{description}</p>
+            <p className=' text-lg text-[#FF0000]'>{price}</p>
           </div>
-  
+          <hr className='border-1 border-[#272424] '></hr>
+          <div className='flex justify-between py-4'>
+              <div className='flex gap-2'>
+                <img src="/icons/calendar.svg" alt="" />
+                <p className='text-md text-bold text-gray-500' >{date}</p>
+              </div>
+              <div className='flex gap-2'>
+                <img src="/icons/gas.svg" alt="" />
+                <p className='text-md text-bold text-gray-500'>{combustivel}</p>
+              </div>
+              <div className='flex gap-2'>
+                <img src="/icons/speed.svg" alt="" />
+                <p className='text-md text-bold text-gray-500'>{kilometragem}</p>
+              </div>
+          </div>
+    
         </div>
       </div>
     );
