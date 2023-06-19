c5ba6463354caedd203e76aa2f174e4498e2d02e
Mega Afu <[mega.afu@champier.co.mz]>
2023-06-19 14:47:05 +0200

    [MotorCare] refact: use mock data for website

---
+++ src/data/cars.ts
@@ -0,0 +1,306 @@
+import { ICar } from "@/core/model/Car";
+interface Cars{
+    cars:ICar[]
+}
+
+
+export const Cars:Cars={
+    cars:[
+        
+        {
+            id:'11',
+            carname:'LEAF',
+            brand:'NISSAN',
+            model:'LEAF',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/leaf/leaf1.jpg',
+                'images/vehicles/leaf/leaf2.jpg',
+                'images/vehicles/leaf/leaf3.jpg',
+                'images/vehicles/leaf/leaf4.jpg',
+                'images/vehicles/leaf/leaf5.jpg',
+                'images/vehicles/leaf/leaf6.jpg',
+                'images/vehicles/leaf/leaf7.jpg',
+                'images/vehicles/leaf/leaf8.jpg',
+                'images/vehicles/leaf/leaf9.jpg',
+                'images/vehicles/leaf/leaf10.jpg',
+                'images/vehicles/leaf/leaf11.jpg',
+            ]
+
+        },
+        {
+            id:'13',
+            carname:'MAGNITE',
+            brand:'NISSAN',
+            model:'MAGNITE',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/magnite/magnite1.jpg',
+                'images/vehicles/magnite/magnite2.jpg',
+                'images/vehicles/magnite/magnite3.jpg',
+            ]
+
+        },
+        
+        {
+            id:'15',
+            carname:'NAVARA PRO-4X',
+            brand:'NISSAN',
+            model:'NAVARA PRO-4X',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 04.jpg',
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 05.jpg',
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 06.jpg',
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 07.jpg',
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 08.jpg',
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 09.jpg',
+                'images/vehicles/navara-pro-4x/Navara PRO-4X - 10.jpg',
+            ]
+
+        },
+        {
+            id:'16',
+            carname:'NQUIGTS Pro',
+            brand:'NISSAN',
+            model:'NQUIGTS Pro',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 01.jpg',
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 02.jpg',
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 03.jpg',
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 04.jpg',
+            ]
+
+        },
+        {
+            id:'17',
+            carname:'NAVARA SC',
+            brand:'NISSAN',
+            model:'NAVARA SC',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 01.jpg',
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 02.jpg',
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 03.jpg',
+                'images/vehicles/navara-sc/Navara - Single Cab Mining - 04.jpg',
+            ]
+
+        },
+        {
+            id:'18',
+            carname:'NAVARA XE DC',
+            brand:'NISSAN',
+            model:'NAVARA XE DC',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/navara-xe/Navara XE DC - 01.jpg',
+                'images/vehicles/navara-xe/Navara XE DC - 02.jpg',
+                'images/vehicles/navara-xe/Navara XE DC - 03.jpg',
+                'images/vehicles/navara-xe/Navara XE DC - 04.jpg',
+                'images/vehicles/navara-xe/Navara XE DC - 05.jpg',
+                'images/vehicles/navara-xe/Navara XE DC - 06.jpg',
+                'images/vehicles/navara-xe/Navara XE DC - 07.jpg',
+            ]
+
+        },
+        {
+            id:'19',
+            carname:'PATROL',
+            brand:'NISSAN',
+            model:'PATROL',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/vehicles/patrol/Patrol 1.jpg',
+                'images/vehicles/patrol/Patrol 2.jpg',
+                'images/vehicles/patrol/Patrol 3.jpg',
+                'images/vehicles/patrol/Patrol 4.jpg',
+            ]
+
+        },
+        {
+            id:'20',
+            carname:'QASHQAI',
+            brand:'NISSAN',
+            model:'QASHQAI',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/scooters/scooter1/side.jpg',
+                'images/scooters/scooter1/front.jpg',
+                'images/scooters/scooter1/back.jpg',
+                'images/scooters/scooter1/backside.jpg',
+                'images/scooters/scooter1/top.jpg',
+            ]
+
+        },
+        {
+            id:'1111',
+            carname:'TERRA',
+            brand:'NIU',
+            model:'TERRA',
+            year:'2019',
+            doors:4,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:4,
+            category:"Vehicles",
+            imageUrl:[
+                'images/scooters/scooter1/side.jpg',
+                'images/scooters/scooter1/front.jpg',
+                'images/scooters/scooter1/back.jpg',
+                'images/scooters/scooter1/backside.jpg',
+                'images/scooters/scooter1/top.jpg',
+            ]
+
+        },
+        {
+            id:'1112',
+            carname:'UQI GT Pro',
+            brand:'NIU',
+            model:'NQUIGTS Pro',
+            year:'2019',
+            doors:0,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:2,
+            category:"Scooters",
+            imageUrl:[
+                'images/scooters/scooter3/side.jpg',
+                'images/scooters/scooter3/front.jpg',
+                'images/scooters/scooter3/back.jpg',
+                'images/scooters/scooter3/backside.jpg',
+                'images/scooters/scooter3/top.jpg',
+            ]
+
+        },
+       
+        {
+            id:'1113',
+            carname:'NQUIGTS Pro',
+            brand:'NIU',
+            model:'NQUIGTS Pro',
+            year:'2019',
+            doors:0,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:2,
+            category:"Scooters",
+            imageUrl:[
+                'images/scooters/scooter3/side.jpg',
+                'images/scooters/scooter3/front.jpg',
+                'images/scooters/scooter3/back.jpg',
+                'images/scooters/scooter3/backside.jpg',
+                'images/scooters/scooter3/top.jpg',
+            ]
+
+        },
+        {
+            id:'1114',
+            carname:'UQI GT Pro',
+            brand:'NIU',
+            model:'NQUIGTS Pro',
+            year:'2019',
+            doors:0,
+            cilinder:'12',
+            fuel:'Diesel',
+            mileage:'180',
+            price:'2,999,990.00',
+            color:'Black',
+            transmission:'Automatic',
+            sits:2,
+            category:"Scooters",
+            imageUrl:[
+                'images/scooters/scooter3/side.jpg',
+                'images/scooters/scooter3/front.jpg',
+                'images/scooters/scooter3/back.jpg',
+                'images/scooters/scooter3/backside.jpg',
+                'images/scooters/scooter3/top.jpg',
+            ]
+
+        },
+    ]
+}
--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,38 +1,48 @@
-interface MenuItem {
+interface IMenu {
     label: string;
-    url: string;
     subMenu: string[];
   }
 
-  export const menu: MenuItem[] = [
-    {
-      label: 'Home',
-      url: '/home',
-      subMenu:[]
-    },
-    {
-      label: 'About Us',
-      url: '/about',
-      subMenu:['Mission','Vision', 'Values', 'History']
-    },
-    {
-      label: 'Vehicles',
-      url: '/cars',
-      subMenu:['New','Pre-Owned','Trucks','Scooter']
-    },
-    {
-      label: 'Parts and Accesories',
-      url: '/',
-      subMenu:['Parts','Tires', 'Accessories']
-    },
-    {
-      label: 'Services',
-      url: '/',
-      subMenu:['Revision','Repair','Diagnosis','Panel Beater and Painting']
-    },
-    {
-      label: 'Contacts',
-      url: '/',
-      subMenu:['Find a Dealer','Complains and Suggestions']
-    },
-  ];
+  export const BrochureMenu:IMenu ={
+        label:"Presented Vehicles",
+        subMenu:[
+            ""
+        ],
+    }
+  export const IsoMenu:IMenu ={
+        label:"ISO Certification",
+        subMenu:[
+            ""
+        ],
+    }
+  export const HistoryMenu:IMenu ={
+      label:"Our History",
+      subMenu:[
+          ""
+      ],
+  }  
+  export const CategoriesMenu:IMenu={
+    label:"Categories",
+    subMenu:["Vehicles", "Trucks", "Scooters"]
+  }
+
+  export const FindCarsMenu:IMenu={
+    label:"Find your Car",
+    subMenu:[ "All","New","Used" ]
+  }
+
+
+  export const BlogMenu:IMenu={
+    label:"Blog",
+    subMenu:["All","Trending", "last Week"]
+  }
+
+  export const AboutMenu:IMenu={
+    label:"About US",
+    subMenu:['Our History', 'Our Brands']
+  }
+
+  export const BrandsMenu:IMenu={
+    label:'Our Brands',
+    subMenu: ['Nissan','Renault','General Tyres', 'Maxxis Tyres', 'CST tyres','Continental']
+  }
