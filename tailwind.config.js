dacd907bfe85d3cfbc2b906e2b46b196dfb20181
Mega Afu <[mega.afu@champier.co.mz]>
2023-06-19 14:49:17 +0200

    [MotorCare] refact: update images and tailwind classes for the pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,18 @@ module.exports = {
       fontFamily:{
         sans: ['var(--font-nissan)']
       },
+      colors:{
+        'primary':'#006bb8',
+        'secundary':'#ff8e00',
+        'ancent':'#002257',
+        'light-text':'#555555',
+      },
       backgroundImage: {
         'hero': "url('../../public/images/bg-hero.png')",
         'brochure': "url('../../public/images/bg-brochure.png')",
         'cars-hero': "url('../../public/images/bg-cars-hero.png')",
         'about-hero': "url('../../public/images/bg-about.png')",
+        'request-hero': "url('../../public/images/bg-request.png')",
         'nissan':"url(../../public/images/nissan_background.png)",
         'informative':"url(../../public/images/informative.png)",
         'footer':"url(../../public/images/footer.png)"
