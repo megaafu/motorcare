06e23e6a175d583e8d4dbe6b15c7b6c016ba0a0c
Mega Afu <[mega.afu@champier.co.mz]>
2023-07-10 10:54:13 +0200

    [MotorCare]refact:(code clean with eslint)

--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -1,44 +1,29 @@
-"use client"
-import React from 'react';
-import Container from '@/components/ui/Container';
-import PagePadding from '@/components/ui/PagePadding';
-import ContentPadding from '@/components/ui/ContentPadding';
-import CarFilter from './components/CarFilter';
-import CarList from './components/CarList';
+import Container from '@/components/ui/Container'
+import ContentPadding from '@/components/ui/ContentPadding'
+import PagePadding from '@/components/ui/PagePadding'
+import CarFilter from './components/CarFilter'
+import CarList from './components/CarList'
 
-
-export default async function UsedCars(){
+export default async function UsedCars() {
   return (
     <main>
-      <div className='bg-cars-hero w-full h-[240px] bg-cover bg-center bg-no-repeat'>
-      </div>
+      <div className="h-[240px] w-full bg-cars-hero bg-cover bg-center bg-no-repeat"></div>
       <Container>
         <PagePadding>
           <div className="">
-            <h1 className="h-[45px] font-normal text-4xl leading-[45px] uppercase text-primary top-72 ">Loren Ipsum</h1>
+            <h1 className="top-72 h-[45px] text-4xl font-normal uppercase leading-[45px] text-primary ">
+              Loren Ipsum
+            </h1>
           </div>
           <div className="mt-4">
-            <hr className="border border-solid border-primary top-80 mt-4 "></hr>
+            <hr className="top-80 mt-4 border border-solid border-primary "></hr>
           </div>
           <ContentPadding>
-            <CarFilter/>
-            
+            <CarFilter />
           </ContentPadding>
-         <CarList/>
+          <CarList />
         </PagePadding>
-       
       </Container>
-
     </main>
-    
-  );
+  )
 }
-
-
-
-
-
-
-
-
-
