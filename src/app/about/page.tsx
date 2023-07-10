25d6cd4f2b5c6a0984617ba32b95f4987b105cf8
Mega Afu <[mega.afu@champier.co.mz]>
2023-07-10 10:29:49 +0200

    [MotorCare] refact:About(implemented lazy load on the page)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,109 +1,139 @@
-"use client"
+'use client'
 
+import { Title } from '@/components/Title'
+import TitleNavItem from '@/components/Title/TitleNavItem'
 import Container from '@/components/ui/Container'
 import ContentPadding from '@/components/ui/ContentPadding'
 import PagePadding from '@/components/ui/PagePadding'
-import React, { useState } from 'react'
-import CustomAccording from './components/According'
-import Row from '../../components/ui/Row'
+import { AboutMenu, HistoryMenu, IsoMenu } from '@/constants/menuData'
+import Image from 'next/image'
+import { useState } from 'react'
 import Paragraph from '../../components/ui/Paragraph'
+import Row from '../../components/ui/Row'
+import CustomAccording from './components/According'
 import CustomTimeLine from './components/CustomTimeLine'
-import { AboutMenu, BrandsMenu, HistoryMenu, IsoMenu } from '@/data/menuData'
-import AnchorMenuItem from '@/components/AnchorMenuItem'
 import NavBrands from './components/NavBrands'
-import SubMenu from '@/components/SubMenu'
-import { History, Holding, Iso } from './data/data'
+import { History, Holding, Iso } from './constants/data'
 
-type Props = {}
-
-function About({}: Props) {
-    const [selectedIndex, setIndex] = useState(0);
-    const handleIndex = (index: number) => {
-        setIndex(index);
-      };
+function About() {
+  const [selectedIndex, setIndex] = useState(0)
+  const handleIndex = (index: number) => {
+    setIndex(index)
+  }
 
   return (
     <main>
-        <div className='bg-about-hero w-full h-[360px] bg-cover bg-center bg-no-repeat'>
-        </div>
-        <Container>
-            <PagePadding>
-                <SubMenu label={AboutMenu.label}>
-                    {AboutMenu.subMenu.map((nav, index) => (
-                        <AnchorMenuItem
-                        key={nav}
-                        label={nav}
-                        isActive={selectedIndex === index}
-                        onClick={() => handleIndex(index)}
-                        />
-                    ))}
-                    
-                </SubMenu>    
-                <ContentPadding>
-                    <Row>
-                        <div> 
-                            <Paragraph>
-                                {Holding.info}
-                            </Paragraph>
-                        </div>
-                        <div className='w-full justify-center items-center'>
-                            <img className='rounded-2xl' src="images/bg-hero.png" alt="img" />
-                        </div>
-                    </Row>
-                </ContentPadding>
-                <ContentPadding>
-                    <Row>
-                        <div className=' relative w-full h-[440px] '>
-                            <img src='images/card-1.png' className=' absolute top-0 right-0  w-[380px] h-[240px] z-0 rounded-3xl opacity-80 border shadow-xl shadow-blue-gray-900/50'  /> 
-                            <img src='images/card-2.png' className=' absolute bottom-32 left-0  w-[380px] h-[240px] z-10 rounded-3xl opacity-90 border shadow-xl shadow-blue-gray-900/50'/> 
-                            <img src='images/bg-about.png' className=' absolute right-16 bottom-0  w-[380px] h-[240px] z-20 rounded-3xl border shadow-xl shadow-blue-gray-900/50'/>    
-                        </div>
-                        <div className=''>
-                            <CustomAccording/>
-                        </div>
-                    </Row>
-                </ContentPadding>
-            </PagePadding>
-            <NavBrands/>
-            <PagePadding>
-                <div id='Our History'>
-                    <SubMenu label={HistoryMenu.label}>
-                        {AboutMenu.subMenu.map((nav, index) => (
-                            <AnchorMenuItem
-                            key={nav}
-                            label={nav}
-                            isActive={selectedIndex === index}
-                            onClick={() => handleIndex(index)}
-                            />
-                        ))}
-                    </SubMenu>    
-                    <ContentPadding>
-                        <Paragraph>
-                            {History.info}
-                        </Paragraph>
-                    </ContentPadding>
-                    <ContentPadding>
-                        <CustomTimeLine/>
-                    </ContentPadding>
-                </div>
-                
-            </PagePadding>
-            <PagePadding>
-                <SubMenu label={IsoMenu.label}/>
-                <ContentPadding>
-                    <Row>
-                        <div>
-                            <Paragraph>
-                                {Iso.info}
-                            </Paragraph>
-                        </div>
-                        <div className='w-full flex justify-center items-center'>
-                            <img className='w-64' src="images/iso.png" alt="img" />
-                        </div>
-                    </Row>
-                </ContentPadding>    
-            </PagePadding>
-        </Container>
+      <div className="h-[360px] w-full bg-about-hero bg-cover bg-center bg-no-repeat"></div>
+      <Container>
+        <PagePadding>
+          <Title.Root>
+            <Title.Label label={AboutMenu.label} />
+            <Title.Nav>
+              {AboutMenu.subMenu.map((nav, index) => (
+                <TitleNavItem
+                  key={nav}
+                  label={nav}
+                  isActive={selectedIndex === index}
+                  onClick={() => handleIndex(index)}
+                />
+              ))}
+            </Title.Nav>
+          </Title.Root>
+          <ContentPadding>
+            <Row>
+              <div>
+                <Paragraph>{Holding.info}</Paragraph>
+              </div>
+              <div className="w-full items-center justify-center">
+                <Image
+                  width={1000}
+                  height={1000}
+                  className="h-full w-full rounded-2xl"
+                  src="/images/bg-hero.png"
+                  alt="img"
+                />
+              </div>
+            </Row>
+          </ContentPadding>
+          <ContentPadding>
+            <Row>
+              <div className=" relative h-[440px] w-full ">
+                <Image
+                  width={1000}
+                  height={1000}
+                  alt="img"
+                  src="/images/card-1.png"
+                  className=" shadow-blue-gray-900/50 absolute right-0  top-0 z-0 h-[240px] w-[380px] rounded-3xl border opacity-80 shadow-xl"
+                />
+                <Image
+                  width={1000}
+                  height={1000}
+                  alt="img"
+                  src="/images/card-2.png"
+                  className=" shadow-blue-gray-900/50 absolute bottom-32  left-0 z-10 h-[240px] w-[380px] rounded-3xl border opacity-90 shadow-xl"
+                />
+                <Image
+                  width={1000}
+                  height={1000}
+                  alt="img"
+                  src="/images/bg-about.png"
+                  className=" shadow-blue-gray-900/50 absolute bottom-0  right-16 z-20 h-[240px] w-[380px] rounded-3xl border shadow-xl"
+                />
+              </div>
+              <div className="">
+                <CustomAccording />
+              </div>
+            </Row>
+          </ContentPadding>
+        </PagePadding>
+        <NavBrands />
+        <PagePadding>
+          <div id="Our History">
+            <Title.Root>
+              <Title.Label label={HistoryMenu.label} />
+              <Title.Nav>
+                {AboutMenu.subMenu.map((nav, index) => (
+                  <TitleNavItem
+                    key={nav}
+                    label={nav}
+                    isActive={selectedIndex === index}
+                    onClick={() => handleIndex(index)}
+                  />
+                ))}
+              </Title.Nav>
+            </Title.Root>
+
+            <ContentPadding>
+              <Paragraph>{History.info}</Paragraph>
+            </ContentPadding>
+            <ContentPadding>
+              <CustomTimeLine />
+            </ContentPadding>
+          </div>
+        </PagePadding>
+        <PagePadding>
+          <Title.Root>
+            <Title.Label label={IsoMenu.label} />
+          </Title.Root>
+
+          <ContentPadding>
+            <Row>
+              <div>
+                <Paragraph>{Iso.info}</Paragraph>
+              </div>
+              <div className="flex w-full items-center justify-center">
+                <Image
+                  width={1000}
+                  height={1000}
+                  className="w-64"
+                  src="/images/iso.png"
+                  alt="img"
+                />
+              </div>
+            </Row>
+          </ContentPadding>
+        </PagePadding>
+      </Container>
     </main>
   )
 }
