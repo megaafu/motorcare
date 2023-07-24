3524d707d6266ed2d09b6c07e3f7ffcb9b9de290
Mega Afu <[mega.afu@champier.co.mz]>
2023-07-24 20:56:09 +0200

    [MotorCare] feat:Service(update the api to prodution)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://api.champier.co.mz/'
+const baseUrl = 'https://api.champier.co.mz/api/v1'
 const api = axios.create({
   baseURL: baseUrl,
   headers: {
