e71d0858bb445a318c0e2e4c058f9c995d118358
Elio Muculo <emuculo25@gmail.com>
2023-08-03 15:29:05 +0200

    [motorcare] fix : endpoint with double slash giving error

--- src/actions/SubmitForm.ts
+++ src/actions/SubmitForm.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export async function SubmitForm(data: any) {
   try {
     const mappedData: any = mapFormData(data);
-    await axios.post(`${BASEURL}/api/v1/statistics`, mappedData).then((Response) => (alert(Response.data)))
+    await axios.post(`${BASEURL}api/v1/statistics`, mappedData).then((Response) => (alert(Response.data)))
 
   } catch (error) {
     alert(error);
