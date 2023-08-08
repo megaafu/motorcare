2a27d5f150746569d1e3b592b4b6b8201dfa7677
Elio Muculo <emuculo25@gmail.com>
2023-08-08 14:14:44 +0200

    [compmoz] fix : skeletion size fixed desktop

--- src/app/[locale]/vehicles/components/CarsSkeletion.tsx
+++ src/app/[locale]/vehicles/components/CarsSkeletion.tsx
@@ -3,15 +3,15 @@ import PagePadding from '@/components/ui/PagePadding'
 import Row from '@/components/ui/Row'
 
 const CarsSkeletion = () => {
-    const totalItems=6 
-    const items = new Array(totalItems).fill(null)
+  const totalItems = 6
+  const items = new Array(totalItems).fill(null)
   return (
     <PagePadding>
-        <Row className="grid-col-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            {items.map((car, index) => (
-                <CarousellCardSkeletion className='mx-auto w-[92vw]'/>
-            ))}
-        </Row>
+      <Row className="grid-col-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {items.map((car, index) => (
+          <CarousellCardSkeletion className='mx-auto w-[92vw] lg:w-auto' />
+        ))}
+      </Row>
     </PagePadding>
   )
 }
--- src/components/Skeletion/CarousellCardSkeletion.tsx
+++ src/components/Skeletion/CarousellCardSkeletion.tsx
@@ -5,12 +5,10 @@ import CardSub from "../Card/CardSub";
 import CardTitle from "../Card/CardTitle";
 
 const CarousellCardSkeletion = ({ className }: { className?: string }) => {
-    const totalItems = 3
-    const items = new Array(totalItems).fill(null)
     return (
-        <CardRoot className={cn("lg:aspect-[4/3]", className)} key={Math.random()}>
+        <CardRoot className="lg:aspect-[4/3]" key={Math.random()}>
             <div className=" aspect-[4/3] bg-slate-500 w-full h-[300px] lg:h-3/4 animate-pulse"></div>
-            <div className="px-4 py-6" key={Math.random()}>
+            <div className={cn("px-4 py-6", className)} key={Math.random()}>
                 <CardTitle>
                     <div className=" h-4 w-2/5 bg-slate-400 rounded animate-pulse"></div>
                 </CardTitle>
