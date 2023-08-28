09bce9e2c5e23f8e82d7ba45597f7e509f4beaf2
Elio Muculo <emuculo25@gmail.com>
2023-08-28 10:15:21 +0200

    [motorcare] fix : merge conflicts

--- src/app/[locale]/vehicles/components/CarsSkeletion.tsx
+++ src/app/[locale]/vehicles/components/CarsSkeletion.tsx
@@ -7,19 +7,11 @@ const CarsSkeletion = () => {
   const items = new Array(totalItems).fill(null)
   return (
     <PagePadding>
-<<<<<<< HEAD
       <Row className="grid-col-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {items.map((car, index) => (
+        {items.map(() => (
           <CarousellCardSkeletion className='mx-auto w-[92vw] lg:w-auto' />
         ))}
       </Row>
-=======
-        <Row className="grid-col-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            {items.map(() => (
-                <CarousellCardSkeletion className='mx-auto w-[92vw] lg:w-auto'/>
-            ))}
-        </Row>
->>>>>>> 08db434 ([MotorCare] Style: Adjust the UI to fit clients needs)
     </PagePadding>
   )
 }
--- src/components/Card/CardButton.tsx
+++ src/components/Card/CardButton.tsx
@@ -3,11 +3,7 @@ import PrimaryButton from "../ui/PrimaryButton"
 
 const CardButton = ({ children }: { children: ReactNode }) => {
   return (
-<<<<<<< HEAD
-    <div className='absolute bottom-5 right-4 md:left-4'>
-=======
     <div className='absolute bottom-5 right-4 md:left-4 '>
->>>>>>> 08db434 ([MotorCare] Style: Adjust the UI to fit clients needs)
       <PrimaryButton className="w-[100%] justify-center lg:w-auto">{children}</PrimaryButton>
     </div>
   )
