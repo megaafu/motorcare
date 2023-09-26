34367cf32e207cbd88755f8bd0deb153b9557112
Mega Afu <[mega.afu@champier.co.mz]>
2023-09-26 10:02:17 +0200

    [MotorCare] style: Added line clap on Cards

--- src/components/Card/CardTitle.tsx
+++ src/components/Card/CardTitle.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 const CardTitle = ({ children }: { children: ReactNode }) => {
-  return <h3 className="text-lg font-bold line-clamp-2">{children}</h3>
+  return <h3 className="text-lg font-bold line-clamp-1">{children}</h3>
 }
 
 export default CardTitle
