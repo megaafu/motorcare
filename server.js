fe78f677e96b0ae0276ec25c105456cb7d1b5e2a
Elio Muculo <emuculo25@gmail.com>
2024-10-02 12:54:09 +0200

    Fix typo in server.js error message

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.prepare().then(() => {
     } catch (err) {
       console.error('Error occurred handling.', req.url, err)
       res.statusCode = 500
-      res.end('internal server error.code ')
+      res.end('internal server error.code.')
     }
   })
     .once('error', (err) => {
