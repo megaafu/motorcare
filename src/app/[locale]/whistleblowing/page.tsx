07c36d3424aaf4167d1fa88f1c6e6dc81ddd8a42
Mega Afu <mega.afu@champier.co.mz>
2025-07-18 12:52:53 +0200

    refact:Update for new dependencies standards

--- src/app/[locale]/whistleblowing/page.tsx
+++ src/app/[locale]/whistleblowing/page.tsx
@@ -6,29 +6,29 @@ import Link from 'next/link'
 import React from 'react'
 
 const WhistleBlowing = () => {
-    const t = useTranslations('Whistleblowing')
+  const t = useTranslations('Whistleblowing')
 
-    return (
-        <main>
-            <Container>
-                <PagePadding>
-                    <section className='grid h-screen  content-center'>
-                        <h4 className='text-4xl text-center'> {t("whistleblowing")} </h4>
-                        <div className='bg-center bg-primary h-1 w-[12vw] mx-auto'></div>
-                        <p className=' max-w-4xl mx-auto text-center mt-4' dangerouslySetInnerHTML={{ __html: t("description").replace(/\n/g, '<br/>') }}>
-                        </p>
-                        <Link
-                            href="https://kjaergroup.integrityline.com/frontpage"
-                            target='blank'
-                            className=' mt-8 bg-primary py-3 px-6 text-white mx-auto'>
-                            {t("report")}
-                        </Link>
-                    </section>
+  return (
+    <main>
+      <Container>
+        <PagePadding>
+          <section className='grid h-screen  content-center'>
+            <h4 className='text-4xl text-center'> {t("whistleblowing")} </h4>
+            <div className='bg-center bg-primary h-1 w-[12vw] mx-auto'></div>
+            <p className=' max-w-4xl mx-auto text-center mt-4' dangerouslySetInnerHTML={{ __html: t("description").replace(/\n/g, '<br/>') }}>
+            </p>
+            <Link
+              href="https://kjaergroup.integrityline.com/frontpage"
+              target='blank'
+              className=' mt-8 bg-primary py-3 px-6 text-white mx-auto'>
+              {t("report")}
+            </Link>
+          </section>
 
-                </PagePadding>
-            </Container>
-        </main>
-    )
+        </PagePadding>
+      </Container>
+    </main>
+  )
 }
 
 export default WhistleBlowing
--- src/lib/util/util.ts
+++ src/lib/util/util.ts
@@ -1,9 +1,10 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 // export const BASEURL = "http://localhost:8000/"
-export const BASEURL = "https://api.champier.co.mz/"
+export const BASEURL = "https://api.champier.co.mz/";
+
