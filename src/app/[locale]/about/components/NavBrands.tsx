e1d91858c9cb889012c1fbbcff1f4144d828c014
Mega Afu <mega.afu@champier.co.mz>
2025-07-18 12:50:59 +0200

    refact:Change to have brands selected automaticaly from url

--- src/app/[locale]/about/components/NavBrands.tsx
+++ src/app/[locale]/about/components/NavBrands.tsx
@@ -9,47 +9,69 @@ import Row from '@/components/ui/Row'
 import { BrandsMenu } from '@/constants/menuData'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { AllBrands } from '../constants/brands'
+import { useSearchParams } from 'next/navigation'
 
 const NavBrands = () => {
   const [selectedIndex, setIndex] = useState(0)
+  const t = useTranslations('About')
+
+  const searchParams = useSearchParams()
+  const brandParam = searchParams.get('brand')
+  const normalize = (value: string) => value.toLowerCase().replace(/\s+/g, '')
+
+  useEffect(() => {
+    if (brandParam) {
+      const foundIndex = AllBrands.brands.findIndex(
+        (item) => normalize(item.brand.toLowerCase()) == normalize(brandParam.toLowerCase())
+      )
+      if (foundIndex !== -1) {
+        setIndex(foundIndex)
+        const target = document.getElementById('brands')
+        target?.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+  }, [brandParam])
+
   const handleIndex = (index: number) => {
     setIndex(index)
+    const target = document.getElementById('brands')
+    target?.scrollIntoView({ behavior: 'smooth' })
   }
-  const t = useTranslations('About')
+
   return (
     <PagePadding>
-      <div id="Our Brands">
+      <div id="brands">
         <Title.Root>
           <Title.Label label={t(BrandsMenu.label)} />
           <Title.Nav>
             <LateralScroll>
-            {AllBrands.brands.map((nav) => (
-              <TitleNavItem
-                key={nav.id}
-                label={nav.brand}
-                isActive={selectedIndex === nav.id}
-                onClick={() => handleIndex(nav.id)}
-              />
-            ))}
+              {AllBrands.brands.map((nav) => (
+                <TitleNavItem
+                  key={nav.id}
+                  label={nav.brand}
+                  isActive={selectedIndex === nav.id}
+                  onClick={() => handleIndex(nav.id)}
+                />
+              ))}
             </LateralScroll>
           </Title.Nav>
         </Title.Root>
         <Row className="lg:grid-cols-2">
-          <div className="">
+          <div>
             <h4 className="text-bold pb-4 text-2xl text-sky-700">
               {AllBrands.brands[selectedIndex].brand}
             </h4>
             <Paragraph>{t(AllBrands.brands[selectedIndex].info)}</Paragraph>
           </div>
-          <div className="flex w-full items-center justify-center ">
+          <div className="flex w-full items-center justify-center">
             <Image
               width={1000}
               height={1000}
               className="w-[280px]"
               src={AllBrands.brands[selectedIndex].img}
-              alt="img"
+              alt="brand image"
             />
           </div>
         </Row>
@@ -59,3 +81,4 @@ const NavBrands = () => {
 }
 
 export default NavBrands
+
