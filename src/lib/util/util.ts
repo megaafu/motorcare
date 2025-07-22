946d084d6030abaf014b51dde119ae58d26802c5
Elio Muculo <emuculo25@gmail.com>
2025-07-22 15:45:52 +0200

    fix: Correct BASEURL assignment for production environment

--- src/lib/util/util.ts
+++ src/lib/util/util.ts
@@ -5,6 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const BASEURL = "http://localhost:8000/"
-// export const BASEURL = "https://api.champier.co.mz/";
+// export const BASEURL = "http://localhost:8000/"
+export const BASEURL = "https://api.champier.co.mz/";
 
