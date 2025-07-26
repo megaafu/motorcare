ffe6ce0a8972380d7da09953662df3df8189061f
megaafu <hanselmega@gmail.com>
2025-07-26 16:50:10 +0200

    build:Update next-intl package and config

--- next.config.js
+++ next.config.js
@@ -1,115 +1,9 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/pt/pt/:path",
-        destination: "/pt/:path",
-        permanent: true,
-      },
-      {
-        source: "/en/pt/:path",
-        destination: "/pt/:path",
-        permanent: true,
-      },
-      {
-        source: "/en/en/:path",
-        destination: "/en/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/en/:path",
-        destination: "/en/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/blogs/pt/:path",
-        destination: "/pt/:path",
-        permanent: true,
-      },
-
-      {
-        source: "/en/blogs/pt/:path",
-        destination: "/pt/:path",
-        permanent: true,
-      },
-      {
-        source: "/en/blogs/en/:path",
-        destination: "/en/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/blogs/en/:path",
-        destination: "/en/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/blogs/pt/blogs/:path",
-        destination: "/pt/blogs/:path",
-        permanent: true,
-      },
-
-      {
-        source: "/en/blogs/pt/blogs/:path",
-        destination: "/pt/blogs/:path",
-        permanent: true,
-      },
-      {
-        source: "/en/blogs/en/blogs/:path",
-        destination: "/en/blogs/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/blogs/en/blogs/:path",
-        destination: "/en/blogs/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/vehicles/pt/:path",
-        destination: "/pt/:path",
-        permanent: true,
-      },
-
-      {
-        source: "/en/vehicles/pt/:path",
-        destination: "/pt/:path",
-        permanent: true,
-      },
-      {
-        source: "/en/vehicles/en/:path",
-        destination: "/en/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/vehicles/en/:path",
-        destination: "/en/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/vehicles/pt/vehicles/:path",
-        destination: "/pt/vehicles/:path",
-        permanent: true,
-      },
-
-      {
-        source: "/en/vehicles/pt/vehicles/:path",
-        destination: "/pt/vehicles/:path",
-        permanent: true,
-      },
-      {
-        source: "/en/vehicles/en/vehicles/:path",
-        destination: "/en/vehicles/:path",
-        permanent: true,
-      },
-      {
-        source: "/pt/vehicles/en/vehicles/:path",
-        destination: "/en/vehicles/:path",
-        permanent: true,
-      },
- 
-    ];
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -120,5 +14,4 @@ const nextConfig = {
     ],
   },
 };
-const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 module.exports = withNextIntl(nextConfig);
