00ad0250338cccc79cf3c57aa08ede2db063b517
megaafu <hanselmega@gmail.com>
2025-07-27 13:20:19 +0200

    build:Update packages, remove @rocketseatlint and configure lint

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = process.env.NODE_ENV !== 'production' ? 'localhost' : 'test.motorcare.co.mz/'
-const port = process.env.port || 3000
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const hostname =
+  process.env.NODE_ENV !== "production" ? "localhost" : "test.motorcare.co.mz/";
+const port = process.env.port || 3000;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
+      if (pathname === "/a") {
+        await app.render(req, res, "/a", query);
+      } else if (pathname === "/b") {
+        await app.render(req, res, "/b", query);
       } else {
-        await handle(req, res, parsedUrl)
+        await handle(req, res, parsedUrl);
       }
     } catch (err) {
-      console.error('Error occurred handling.', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error.code.')
+      console.error("Error occurred handling.", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error.code.");
     }
   })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
     })
     .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
-})
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-nissan)'],
+        sans: ["var(--font-nissan)"],
       },
       colors: {
-        primary: '#5d5151',
-        secundary: '#F8B067',
-        ancent: '#443b3b',
-        'light-text': '#2B415A',
+        primary: "#5d5151",
+        secundary: "#F8B067",
+        ancent: "#443b3b",
+        "light-text": "#2B415A",
       },
       backgroundImage: {
         hero: "url('/images/bg-hero.png')",
         brochure: "url('/images/bg-brochure.png')",
-        'cars-hero': "url('/images/bg-cars-hero.png')",
-        'about-hero': "url('/images/bg-about.JPG')",
-        'request-hero': "url('/images/bg-request.png')",
+        "cars-hero": "url('/images/bg-cars-hero.png')",
+        "about-hero": "url('/images/bg-about.JPG')",
+        "request-hero": "url('/images/bg-request.png')",
         nissan: "url('/images/nissan_background.png')",
         informative: "url('/images/informative.png')",
         footer: "url('/images/footer.png')",
@@ -29,4 +29,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
