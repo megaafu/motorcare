8ab078396966e2e968c938044bad307af35b4c0a
Mega Afu <hanselmega@gmail.com>
2025-09-26 10:25:40 +0200

    style: Add Infinite slide to Brands

--- src/app/[locale]/home/components/BrandCarousel.tsx
+++ src/app/[locale]/home/components/BrandCarousel.tsx
@@ -1,25 +1,18 @@
 "use client";
 
 import Image from "next/image";
-import useEmblaCarousel from "embla-carousel-react";
-import Autoplay from "embla-carousel-autoplay";
 import { AllBrands } from "../../about/constants/brands";
 
 export default function BrandsCarousel() {
-
-  const [emblaRef] = useEmblaCarousel(
-    { loop: true, align: "start" },
-    [Autoplay({ delay: 3000 })]
-  );
-
   return (
-    <div className="w-full py-10">
-      <div className="overflow-hidden" ref={emblaRef}>
-        <div className="flex">
+    <div className="w-full py-10 overflow-hidden">
+      <div className="relative flex">
+        {/* Track wrapper with infinite animation */}
+        <div className="flex animate-marquee">
           {[...AllBrands.brands, ...AllBrands.brands].map((brand, index) => (
             <div
               key={index}
-              className="flex-shrink-0 w-60 h-60 flex items-center justify-center p-4 "
+              className="flex-shrink-0 w-60 h-60 flex items-center justify-center p-4"
             >
               <div className="relative w-full bg-gray-100 h-full shadow-lg rounded-md">
                 <Image
@@ -36,4 +29,3 @@ export default function BrandsCarousel() {
     </div>
   );
 }
-
